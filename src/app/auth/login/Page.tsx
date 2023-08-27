029df7d0e8019da535f30d13679b636a75da2b17
sammykirigha <dkirigha18@gmail.com>
2023-08-27 12:31:29 +0300

    made some changes

--- src/app/auth/login/Page.tsx
+++ src/app/auth/login/Page.tsx
@@ -2,12 +2,12 @@ import Navbar from "@/src/components/Navbar";
 import Login from "@/src/components/auth/Login";
 
 function LoginPage() {
-  return (
-    <div className="min-h-[100vh] bg-zinc-200 flex items-center justify-center">
-      <Navbar />
-      <Login />
-    </div>
-  );
+    return (
+        <div className="min-h-[100vh] bg-zinc-200 flex items-center justify-center">
+            <Navbar />
+            <Login />
+        </div>
+    );
 };
 
 export default LoginPage;
